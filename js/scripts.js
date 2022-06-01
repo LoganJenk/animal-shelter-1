4eccd827e4e98a519e76b682d168c841829e0067
Logan Jenkins <LoganJenk1@gmail.com>
2022-06-01 16:20:58 -0700

    add dark/light background buttons to body

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("button#light").click(function() {
+        $("body").removeClass();
+        $("body").addClass("light-background");
+    });
+
+    $("button#dark").click(function() {
+        $("body").removeClass();
+        $("body").addClass("dark-background");
+    });
+
+    $("button#original").click(function() {
+        $("body").removeClass()
+    });
+});
